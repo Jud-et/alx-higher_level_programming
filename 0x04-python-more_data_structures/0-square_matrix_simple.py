1220df0bbc0112f4bfaa63fa09e5982979fe707e
Jud-et <judithk859@gmail.com>
2023-08-16 06:17:10 -0700

    0. Squared simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,18 @@
+#!usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    # Create a new matrix to store squared values
+    squared_matrix = []
+
+    # Iterate over each row in the input matrix
+    for row in matrix:
+        # Create a new row to store squared values
+        squared_row = []
+
+        # Iterate over each element in the row and compute the square
+        for num in row:
+            squared_row.append(num ** 2)
+
+        # Add the squared row to the squared matrix
+        squared_matrix.append(squared_row)
+
+    return squared_matrix
