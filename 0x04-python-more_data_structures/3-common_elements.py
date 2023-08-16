f77021dbc10969c1ba41d326dbbde470f8d2419c
Jud-et <judithk859@gmail.com>
2023-08-16 06:29:15 -0700

    3. Present in both

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+ef common_elements(set_1, set_2):
+    # Create an empty set to store common elements
+    common_set = set()
+
+    # Iterate through the elements in set_1
+    for item in set_1:
+        # Check if the element exists in set_2
+        if item in set_2:
+            common_set.add(item)
+
+    return common_set
+
